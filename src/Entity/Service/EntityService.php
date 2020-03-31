35ca5a2e123749f79bbe66df31b519ee6ad98ea4
Hamed Darragi <hd@xanweb.com>
2020-03-31 09:33:44 +0100

    Add EntityService that helps with entities CRUD

---
+++ src/Entity/Service/EntityService.php
@@ -0,0 +1,151 @@
+<?php
+namespace Xanweb\Foundation\Entity\Service;
+
+use Doctrine\ORM\EntityManagerInterface;
+use Concrete\Core\Support\Facade\Application;
+
+abstract class EntityService implements ServiceInterface
+{
+    /**
+     * @var EntityManagerInterface
+     */
+    protected $entityManager;
+
+    /**
+     * @var \Doctrine\ORM\EntityRepository
+     */
+    private $repo;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * Gets the repository for the entity class.
+     *
+     * @return \Doctrine\ORM\EntityRepository
+     */
+    protected function repo()
+    {
+        if (!$this->repo) {
+            $this->repo = $this->entityManager->getRepository($this->getEntityClass());
+        }
+
+        return $this->repo;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::createEntity()
+     */
+    public function createEntity()
+    {
+        $app = Application::getFacadeApplication();
+        return $app->make($this->getEntityClass());
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::getList()
+     */
+    public function getList()
+    {
+        return $this->repo()->findAll();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::getSortedList()
+     */
+    public function getSortedList($orderBy = [])
+    {
+        return $this->repo()->findBy([], $orderBy);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::getByID()
+     */
+    public function getByID($id)
+    {
+        return $this->repo()->find($id);
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::create()
+     */
+    public function create(array $data = [])
+    {
+        $entity = $this->createEntity();
+        $entity->setPropertiesFromArray($data);
+
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+
+        return $entity;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::update()
+     */
+    public function update($entity, array $data = [])
+    {
+        $entity->setPropertiesFromArray($data);
+
+        $this->entityManager->persist($entity);
+        $this->entityManager->flush();
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::bulkSave()
+     */
+    public function bulkSave(array $entities)
+    {
+        foreach ($entities as $entity) {
+            $this->entityManager->persist($entity);
+        }
+
+        $this->entityManager->flush();
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::delete()
+     */
+    public function delete($entity)
+    {
+        $this->entityManager->remove($entity);
+        $this->entityManager->flush();
+
+        return true;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see ServiceInterface::bulkDelete()
+     */
+    public function bulkDelete(array $entities)
+    {
+        foreach ($entities as $entity) {
+            $this->entityManager->remove($entity);
+        }
+        $this->entityManager->flush();
+
+        return true;
+    }
+}
---
+++ src/Entity/Service/ServiceInterface.php
@@ -0,0 +1,86 @@
+<?php
+namespace Xanweb\Foundation\Entity\Service;
+
+interface ServiceInterface
+{
+    /**
+     * Get full entity class name.
+     *
+     * @return string
+     */
+    public function getEntityClass();
+
+    /**
+     * Create New Entity instance.
+     *
+     * @return \Xanweb\Foundation\ConcreteObject Entity object
+     */
+    public function createEntity();
+
+    /**
+     * Finds all entities in the repository.
+     *
+     * @return \Xanweb\Foundation\ConcreteObject[] the entities
+     */
+    public function getList();
+
+    /**
+     * Finds all entities in the repository sorted by given fields.
+     *
+     * @param array $orderBy
+     *
+     * @return \Xanweb\Foundation\ConcreteObject[] the entities
+     */
+    public function getSortedList($orderBy = []);
+
+    /**
+     * Finds the entity by its primary key / identifier.
+     *
+     * @param mixed    $id          The identifier
+     *
+     * @return \Xanweb\Foundation\ConcreteObject|null The entity instance or NULL if the entity can not be found
+     */
+    public function getByID($id);
+
+    /**
+     * Create Entity.
+     *
+     * @param array $data
+     *
+     * @return \Xanweb\Foundation\ConcreteObject
+     */
+    public function create(array $data = []);
+
+    /**
+     * Update Entity.
+     *
+     * @param \Xanweb\Foundation\ConcreteObject $entity
+     * @param array $data
+     *
+     * @return bool
+     */
+    public function update($entity, array $data = []);
+
+    /**
+     * Persist a list of entities and flush all changes.
+     *
+     * @param array $entities
+     */
+    public function bulkSave(array $entities);
+
+    /**
+     * Delete Entity.
+     *
+     * @param \Xanweb\Foundation\ConcreteObject $entity
+     *
+     * @return bool
+     */
+    public function delete($entity);
+
+    /**
+     * Delete a list of entities and flush all changes.
+     *
+     * @param array $entities
+     */
+    public function bulkDelete(array $entities);
+}
