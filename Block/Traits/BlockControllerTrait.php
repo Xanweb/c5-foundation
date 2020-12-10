6b141132db74364eed5df9700cebac82cc1a4647
Hamed Darragi <hd@xanweb.com>
2020-12-10 14:08:08 +0100

    Introduce new methods in BlockControllerTrait to get related page object and page language

--- Block/Traits/BlockControllerTrait.php
+++ Block/Traits/BlockControllerTrait.php
@@ -2,10 +2,15 @@
 namespace Xanweb\Foundation\Block\Traits;
 
 use Concrete\Core\Block\Block;
+use Concrete\Core\Multilingual\Page\Section\Section;
+use Concrete\Core\Page\Page;
+use Concrete\Core\Page\Stack\Stack;
 use Concrete\Core\Permission\Checker as Permissions;
 use Concrete\Core\Session\SessionValidator;
 use Concrete\Core\Utility\Service\Validation\Numbers;
 use Illuminate\Support\Str;
+use Xanweb\Foundation\Request\User as RequestUser;
+use Xanweb\Foundation\Request\Page as RequestPage;
 
 /**
  * Trait BlockControllerTrait.
@@ -17,6 +22,11 @@ use Illuminate\Support\Str;
  */
 trait BlockControllerTrait
 {
+    /**
+     * @var Page
+     */
+    private $blockPage;
+
     protected $realIdentifier;
     protected $uniqID;
 
@@ -67,18 +77,26 @@ trait BlockControllerTrait
         return false;
     }
 
-    /**
-     * return current block area.
-     */
-    protected function getCurrentAreaName(): string
+    protected function getCurrentAreaName(): ?string
     {
-        if ($this->block instanceof Block) {
-            $areaName = $this->block->getAreaHandle();
-        } else {
-            $areaName = $this->request->get('arHandle');
+        $areaName = ($this->block instanceof Block) ? $this->block->getAreaHandle() : null;
+
+        return $areaName ?? $this->request->get('arHandle');
+    }
+
+    /**
+     * Check whether we are in Edit Mode.
+     *
+     * @return bool
+     */
+    public function isInEditMode(): bool
+    {
+        $c = $this->getPageObject();
+        if ($c !== null) {
+            return $c->isEditMode();
         }
 
-        return $areaName;
+        return RequestPage::isEditMode();
     }
 
     /**
@@ -124,5 +142,91 @@ trait BlockControllerTrait
     {
         $session = $this->app->make('session');
         $session->getFlashBag()->add('block_message_' . $this->getRealIdentifier(), [$key, $value, $isHTML]);
+    }
+
+    /**
+     * Get Block Related Page Language.
+     *
+     * @return string
+     */
+    public function getPageLanguage(): string
+    {
+        static $language;
+
+        if (!$language && ($page = $this->getPageObject()) !== null) {
+            $section = Section::getBySectionOfSite($page);
+            if (is_object($section)) {
+                $language = $section->getLanguage();
+            }
+        }
+
+        return $language ?? RequestPage::getLanguage();
+    }
+
+    /**
+     * Get Block Related Page Locale.
+     *
+     * @return string
+     */
+    public function getPageLocale(): string
+    {
+        static $locale;
+
+        if (!$locale && ($page = $this->getPageObject()) !== null) {
+            $section = Section::getBySectionOfSite($page);
+            if (is_object($section)) {
+                $locale = $section->getLocale();
+            }
+        }
+
+        return $locale ?? RequestPage::getLocale();
+    }
+
+    /**
+     * Get Block Related Page Object.
+     *
+     * @return Page
+     */
+    public function getPageObject(): ?Page
+    {
+        if (!$this->blockPage) {
+            if ($this->isEditedWithinStack()) {
+                $this->blockPage = $this->getCollectionObject() ?: null;
+            } else {
+                $c = $this->request->getCurrentPage();
+                if ($c instanceof Page && !$c->isError()) {
+                    $this->blockPage = $c;
+                } else {
+                    $this->blockPage = $this->getCollectionObject() ?: null;
+                }
+            }
+        }
+
+        return $this->blockPage;
+    }
+
+    /**
+     * Check if the block is edited in Stack.
+     *
+     * @return bool
+     */
+    public function isEditedWithinStack(): bool
+    {
+        if (RequestUser::canAccessDashboard() && !empty($path = $this->request->getPath())
+            && $this->request->matches('*' . STACKS_LISTING_PAGE_PATH . '*')) {
+
+            $cID = (string) last(explode('/', $path));
+            if (strpos($cID, '@') !== false) {
+                list($cID, $locale) = explode('@', $cID, 2);
+            }
+
+            if ($cID > 0) {
+                $s = Stack::getByID($cID);
+
+                return is_object($s);
+            }
+        }
+
+        return false;
     }
 }
