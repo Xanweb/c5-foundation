1491031cbb14f68f9eeb37d2885dafab47933a76
Hamed Darragi <hd@xanweb.com>
2021-03-22 15:32:25 +0100

    Simplify route declaration

--- Route/RouteList.php
+++ Route/RouteList.php
@@ -3,15 +3,12 @@ namespace Xanweb\Foundation\Route;
 
 use Concrete\Core\Routing\RouteListInterface;
 use Concrete\Core\Routing\Router;
+use Xanweb\Foundation\Controller\JavascriptAssetDefaults;
 
 class RouteList implements RouteListInterface
 {
     public function loadRoutes(Router $router): void
     {
-        $router->buildGroup()
-            ->setNamespace('Xanweb\Foundation\Controller')
-            ->routes(function (Router $r) {
-                $r->get('/xw/js/defaults.js', 'JavascriptAssetDefaults::getJavascript');
-            });
+        $router->get('/xw/js/defaults.js', JavascriptAssetDefaults::class . '::getJavascript');
     }
 }
