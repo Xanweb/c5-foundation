c9693317ef971b92d649646951245d423d1a8224
Hamed Darragi <hd@xanweb.com>
2021-03-31 10:32:21 +0100

    Remove deprecated classes

--- Entity/Service/EntityService.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Xanweb\Foundation\Entity\Service;
-
-/**
- * @deprecated use \Xanweb\C5\Entity\Service\EntityService
- */
-abstract class EntityService extends \Xanweb\C5\Entity\Service\EntityService implements ServiceInterface
-{
-}
