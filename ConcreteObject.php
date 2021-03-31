7ce30c7e5a779cf1e5371a9fb6477c9759c24b33
Hamed Darragi <hd@xanweb.com>
2021-03-31 10:31:03 +0100

    Use new common traits for ConcreteObject

--- ConcreteObject.php
+++ ConcreteObject.php
@@ -3,45 +3,11 @@
 namespace Xanweb\Foundation;
 
 use Concrete\Core\Foundation\ConcreteObject as CoreObject;
-use Doctrine\Common\Collections\ArrayCollection;
+use Xanweb\Common\Traits\JsonSerializableTrait;
+use Xanweb\Common\Traits\ObjectTrait;
 
 abstract class ConcreteObject extends CoreObject implements \JsonSerializable
 {
-    public function setPropertiesFromArray($arr)
-    {
-        foreach ($arr as $key => $prop) {
-            $setter = 'set' . ucfirst($key);
-            // we prefer passing by setter method
-            if (method_exists($this, $setter)) {
-                $this->$setter($prop);
-            } else {
-                $this->{$key} = $prop;
-            }
-        }
-    }
-
-    public function jsonSerialize()
-    {
-        $dh = c5app('date');
-        $jsonObj = new \stdClass();
-        $array = get_object_vars($this);
-        foreach ($array as $key => $v) {
-            if ($v && ($v instanceof \DateTimeInterface)) {
-                $jsonObj->{$key} = $dh->formatDate($v);
-            } elseif (is_object($v)) {
-                $this->jsonSerializeRelatedObj($key, $v, $jsonObj);
-            } else {
-                $jsonObj->{$key} = $v;
-            }
-        }
-
-        return $jsonObj;
-    }
-
-    protected function jsonSerializeRelatedObj($key, $o, $jsonObj): void
-    {
-        if (!($o instanceof ArrayCollection) && method_exists($o, 'getID')) {
-            $jsonObj->{$key . 'ID'} = $o->getID();
-        }
-    }
+    use ObjectTrait;
+    use JsonSerializableTrait;
 }
