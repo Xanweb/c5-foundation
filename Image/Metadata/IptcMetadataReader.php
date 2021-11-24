1a13066ff149e185493c40bbcd67a0f794b1db4d
Hamed Darragi <hd@xanweb.com>
2021-11-24 12:22:43 +0100

    Format code

--- Image/Metadata/IptcMetadataReader.php
+++ Image/Metadata/IptcMetadataReader.php
@@ -96,25 +96,25 @@ class IptcMetadataReader extends AbstractMetadataReader
     private function getIptcKeys(): array
     {
         return [
-            '2#005'=>'document_title',
-            '2#010'=>'urgency',
-            '2#015'=>'category',
-            '2#020'=>'subcategories',
-            '2#040'=>'special_instructions',
-            '2#055'=>'creation_date',
-            '2#080'=>'author_byline',
-            '2#085'=>'author_title',
-            '2#090'=>'city',
-            '2#095'=>'state',
-            '2#101'=>'country',
-            '2#103'=>'otr',
-            '2#105'=>'headline',
-            '2#110'=>'source',
-            '2#115'=>'photo_source',
-            '2#116'=>'copyright',
-            '2#120'=>'caption',
-            '2#122'=>'caption_writer',
-            '2#025'=>'keywords'
+            '2#005' => 'document_title',
+            '2#010' => 'urgency',
+            '2#015' => 'category',
+            '2#020' => 'subcategories',
+            '2#040' => 'special_instructions',
+            '2#055' => 'creation_date',
+            '2#080' => 'author_byline',
+            '2#085' => 'author_title',
+            '2#090' => 'city',
+            '2#095' => 'state',
+            '2#101' => 'country',
+            '2#103' => 'otr',
+            '2#105' => 'headline',
+            '2#110' => 'source',
+            '2#115' => 'photo_source',
+            '2#116' => 'copyright',
+            '2#120' => 'caption',
+            '2#122' => 'caption_writer',
+            '2#025' => 'keywords'
         ];
     }
 }
